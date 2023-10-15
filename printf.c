a7aee4f61012d58aeae97af5c4177e14b994157d
Ompheletse Makwela <omphentse@gmail.com>
2023-10-15 06:51:21 -0700

    second commit

--- printf.c
+++ printf.c
@@ -16,7 +16,20 @@ int _strlen(char *c)
 	{
 		length++;
 	}
-	return (lenght);
+	return (length);
+}
+
+/**
+ * _string - String func
+ * @str: A pointer to a character
+ * Return: The string length
+ */
+int _string(char *str)
+{
+	int length = _strlen(str);
+
+	write(1, str, length);
+	return (length);
 }
 
 /**
@@ -29,6 +42,7 @@ int _printf(const char *format, ...)
 	va_list arg;
 	int i;
 	int count = 0;
+
 	va_start(arg, format);
 
 	for (i = 0; format[i] != '\0'; i++)
@@ -39,28 +53,30 @@ int _printf(const char *format, ...)
 			if (format[i] == 'c')
 			{
 				char c = va_arg(arg, int);
+
 				write(1, &c, 1);
 				count++;
 			}
 			else if (format[i] == 's')
 			{
 				char *str = va_arg(arg, char *);
-				int length = _strlen(str);
-				write(1,str,length);
-				count += length;
+
+				count += _string(str);
 			}
 			else if (format[i] == '%')
 			{
 				char p = '%';
+
 				write(1, &p, 1);
 				count++;
 			}
+			else
+			{
+				write(1, &format[i], 1);
+				count++;
+			}
 		}
-		else
-		{
-			write(1, &format[i], 1);
-			count++;
-		}
-		va_end(arg);
-		return (count);
 	}
+			va_end(arg);
+			return (count);
+}
